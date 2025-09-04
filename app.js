f3062fbe46c2e4bd80751c56232da944e8ec7f1f
Alison Pong <alison.pong@gmail.com>
2025-09-04 22:09:31 +0800

    added flash middleware

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
 
